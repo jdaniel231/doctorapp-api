003ded433ed10819f7f9cac7d85709ba841f7c65
jdaniel231 <jdaniel231@gmail.com>
2024-08-18 21:57:48 -0300

    api authorization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
+
+// Login route
 router.post("/login", userController.login);
+
+// Register route
 router.post("/register", userController.register);
+
+//Auth route
+router.post("/getUserData", authMiddleware, userController.auth);
+
 
 module.exports = router;
